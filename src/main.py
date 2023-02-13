bc6f280bf766cf6e06b56cd830d1eebad2d373ef
KangKang <44329867+Cjjj-sys@users.noreply.github.com>
2023-02-13 15:04:09 +0000

    在model中添加UserData类 & 添加 send_email 方法(未实现)

--- src/main.py
+++ src/main.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 # 视图函数（路由）
 @app.route('/')
 def index():
-	return 'Hello World'
+    return 'Hello World'
+
+@app.route('/post/<str:email>')
+def send_email(email: str):
+    return 'nothing'
 
 if __name__ == '__main__':
-	app.run("127.0.0.1",5418)
+    app.run("127.0.0.1", 5418)
---
+++ src/models/user_data.py
@@ -0,0 +1,9 @@
+"""The user data model
+    nothing special...
+"""
+class UserData:
+    """The user data model
+    """
+
+    name: str
+    email: str
